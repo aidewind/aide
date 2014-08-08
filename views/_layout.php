74ba6cbc58accec16f43960cce111dd138db7cc8
aidewind <aidewind@gmail.com>
2014-08-08 06:28:40 -0300

    it s time

--- views/_layout.php
+++ views/_layout.php
@@ -53,10 +53,25 @@
 <div class="navbar navbar-default" id="subnav">
   <div class="col-md-12">
     <div class="navbar-header">
-      <a href="#" style="margin-left:15px;" class="navbar-btn btn btn-default btn-plus dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-home" style="color:#dd1111;"></i> Home <small><i class="glyphicon glyphicon-chevron-down"></i></small></a>
+      <a href="#" style="margin-left:15px;" class="navbar-btn btn btn-default btn-plus dropdown-toggle" data-toggle="dropdown">
+        <?php 
+        if(!$session->account){
+          echo '<i class="glyphicon glyphicon-home" style="color:#dd1111;"></i>';
+        }else{
+          echo $session->account;
+        }?>
+      <small><i class="glyphicon glyphicon-chevron-down"></i></small></a>
       <ul class="nav dropdown-menu">
+        <?php 
+        if(!$session->account){?>
         <li><a href="<?php echo $this->route_url('signin', 'account'); ?>"><i class="glyphicon glyphicon-user" style="color:#1111dd;"></i> Sign In</a></li>
         <li><a href="<?php echo $this->route_url('signup', 'account'); ?>"><i class="glyphicon glyphicon-plus"></i> Sign Up</a></li>
+        <?php }else{ ?>
+        <li><a href="<?php echo $this->route_url('signout', 'account'); ?>"><i class="glyphicon glyphicon-user" style="color:#1111dd;"></i> Sign Out</a></li>
+        <li><a href="<?php echo $this->route_url(NULL, 'ticket'); ?>"><i class="glyphicon glyphicon-inbox" style="color:#11dd11;"></i> My Tickets</a></li>
+        <li><a href="<?php echo $this->route_url('password', 'account'); ?>"><i class="glyphicon glyphicon-inbox" style="color:#11dd11;"></i> Update Password</a></li>
+        <li><a href="<?php echo $this->route_url('settings', 'account'); ?>"><i class="glyphicon glyphicon-cog" style="color:#dd1111;"></i> Settings</a></li>
+        <?php } ?>
       </ul>
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse2">
         <span class="sr-only">Toggle navigation</span>
@@ -68,6 +83,11 @@
     <div class="collapse navbar-collapse" id="navbar-collapse2">
       <ul class="nav navbar-nav navbar-right">
         <li class="active"><a href="<?php echo $this->route_url(NULL, 'home'); ?>">~public</a></li>
+        <?php 
+          foreach ($sectors as $key => $value) {
+            echo '<li><a href="#">~'.$value.'</a></li>';
+          }
+        ?>
       </ul>
     </div>  
   </div> 
--- views/ticket/edit.php
+++ views/ticket/edit.php
@@ -1,27 +1,3 @@
-<div class="navbar navbar-default" id="subnav">
-  <div class="col-md-12">
-    <div class="navbar-header">
-      <a href="#" style="margin-left:15px;" class="navbar-btn btn btn-default btn-plus dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-home" style="color:#dd1111;"></i> General Settings <small><i class="glyphicon glyphicon-chevron-down"></i></small></a>
-      <ul class="nav dropdown-menu">
-        <li><a href="<?php echo $this->route_url('password','admin'); ?>"><i class="glyphicon glyphicon-inbox" style="color:#11dd11;"></i> Update Password</a></li>
-        <li><a href="<?php echo $this->route_url('settings','admin'); ?>"><i class="glyphicon glyphicon-cog" style="color:#dd1111;"></i> Settings</a></li>
-      </ul>
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse2">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="navbar-collapse2">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?php echo $this->route_url(NULL,'admin'); ?>">~admin</a></li>
-        <?php //$this->render_boards(); ?>
-      </ul>
-    </div>  
-  </div> 
-</div>  
-
 <!--main-->
 <div class="container" id="main">
   <div class="row">
