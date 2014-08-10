266622bbcd7eb5d61cd228b06907aa8b2823ffbe
aidewind <aidewind@gmail.com>
2014-08-10 16:11:52 -0300

    Where is the error

--- controllers/sector.php
+++ controllers/sector.php
@@ -12,7 +12,7 @@ class SectorController extends Controller {
     
     $settings = $this->get_settings();
     
-    $sectors = sector_closure::select_descendants_of($id);
+    $sectors = closure::select_descendants_of($id);
 
     //var_dump($sectors);
     //stop();
