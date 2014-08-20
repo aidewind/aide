63bd980341218e76f748ec58cca368639d85fbba
aidewind <aidewind@gmail.com>
2014-08-20 12:05:47 -0300

    using exim4 to send emails

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ if dpkg -s "apache" > /dev/null 2>&1; then
   exit 1
 fi
 
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove
+
 
 echo "prerequisites installing"
 
@@ -19,7 +21,7 @@ mysql-server
 nginx
 php5-fpm
 php5-mysql
-sendmail
+exim4
 "
 
 for pak in $PACKAGE_LIST ; do
@@ -36,6 +38,11 @@ rm -rf master.zip* aide-master*
 wget https://github.com/aidewind/aide/archive/master.zip > /dev/null 2>&1
 unzip master.zip > /dev/null 2>&1
 
+echo "configuring exim4"
+#font https://www.digitalocean.com/community/tutorials/how-to-install-the-send-only-mail-server-exim-on-ubuntu-12-04
+sudo mv aide-master/etc/exim4/update-exim4.conf.conf etc/exim4/update-exim4.conf.conf
+
+echo "configuring nginx"
 sudo mv aide-master/etc/nginx/sites-available/default /etc/nginx/sites-available/default
 sudo rm -rf aide-master/etc aide-master/home aide-master/opt
 
@@ -44,16 +51,17 @@ sudo cp -rf aide-master/* /usr/share/nginx/html
 
 sudo chmod 755 -R /usr/share/nginx/html
 
-echo "configuring sendmail"
-sudo sed -i '/127.0.0.1\tlocalhost/c\127.0.0.1\tlocalhost.localdomain localhost'  /etc/hosts
-sudo sed -i '/127.0.0.1\t'$HOSTNAME'/c\127.0.0.1\t'$HOSTNAME'.localhost '$HOSTNAME  /etc/hosts
-echo "define('SMART_HOST','smtp.spbtlg.ru')dnl" | sudo tee -a /etc/mail/sendmail.mc
-cd /etc/mail
-sudo su
-m4 sendmail.mc > sendmail.cf
-make
-exit
-sudo /etc/init.d/sendmail restart
+#echo "configuring sendmail"
+#sudo sed -i '/127.0.0.1\tlocalhost/c\127.0.0.1\tlocalhost.localdomain localhost'  /etc/hosts
+#sudo sed -i '/127.0.0.1\t'$HOSTNAME'/c\127.0.0.1\t'$HOSTNAME'.localhost '$HOSTNAME  /etc/hosts
+#echo "define('SMART_HOST','smtp.spbtlg.ru')dnl" | sudo tee -a /etc/mail/sendmail.mc
+#cd /etc/mail
+#sudo su
+#m4 sendmail.mc > sendmail.cf
+#make
+#exit
+#sudo /etc/init.d/sendmail restart
+
 
 echo "nginx restarting"
 sudo nginx -s stop 
